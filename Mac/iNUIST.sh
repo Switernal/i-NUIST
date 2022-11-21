090e1cbc8c97d5f984f69763c5e15df5db79fbca
Switernal <switernal@outlook.com>
2022-11-21 17:52:37 +0800

    更新登录页面ip地址至10.255.255.46

--- Mac/iNUIST.sh
+++ Mac/iNUIST.sh
@@ -33,7 +33,7 @@ isAutoLogin=0
 # 获取ip
 
 # ip请求结果json存到./ipResult.json
-ipRes=`curl -H 'Host: 10.255.255.34' -H 'Accept: application/json, text/plain, */*' -H 'User-Agent: Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36' -H 'Referer: http://10.255.255.34/authentication' -H 'Accept-Language: zh-CN,zh;q=0.9' --compressed 'http://10.255.255.34/api/v1/ip' > ipResult.json`
+ipRes=`curl -H 'Host: 10.255.255.46' -H 'Accept: application/json, text/plain, */*' -H 'User-Agent: Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36' -H 'Referer: http://10.255.255.46/authentication' -H 'Accept-Language: zh-CN,zh;q=0.9' --compressed 'http://10.255.255.46/api/v1/ip' > ipResult.json`
 
 # 解析json得到ip
 ip=`jq .data ipResult.json | awk -F \" '{print $2}' ` 
@@ -43,7 +43,7 @@ rm ipResult.json
 echo $ip
 
 # 发起登录请求，结果暂存到./loginResult.json
-loginQuery=`curl -H 'Host: 10.255.255.34' -H 'Accept: application/json, text/plain, */*' -H 'User-Agent: Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36' -H 'Content-Type: application/json;charset=UTF-8' -H 'Origin: http://10.255.255.34' -H 'Referer: http://10.255.255.34/authentication/login' -H 'Accept-Language: zh-CN,zh;q=0.9' --data-binary '{"username":"'$username'","password":"'$password'","channel":"'$channel'","ifautologin":"'$isAutoLogin'","pagesign":"secondauth","usripadd":"'$ip'"}' --compressed 'http://10.255.255.34/api/v1/login' > loginResult.json`
+loginQuery=`curl -H 'Host: 10.255.255.46' -H 'Accept: application/json, text/plain, */*' -H 'User-Agent: Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36' -H 'Content-Type: application/json;charset=UTF-8' -H 'Origin: http://10.255.255.46' -H 'Referer: http://10.255.255.46/authentication/login' -H 'Accept-Language: zh-CN,zh;q=0.9' --data-binary '{"username":"'$username'","password":"'$password'","channel":"'$channel'","ifautologin":"'$isAutoLogin'","pagesign":"secondauth","usripadd":"'$ip'"}' --compressed 'http://10.255.255.46/api/v1/login' > loginResult.json`
 
 # 打印登录结果
 echo "\n 登录结果"
